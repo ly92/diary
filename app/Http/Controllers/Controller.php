8e191acae4cdfacbbd68f0dce2586d49167a42a8
ly <1364757394@qq.com>
2019-03-28 17:57:12 +0800

    123

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,7 +9,15 @@ class Controller extends BaseController
     
 
 	public function hook(){
-		return '123';
+		//查看当前账户，我服务器用的是nginx，所以这里返回的用户是‘nginx’
+        // system('whoami');
+
+        //重定位
+        system('sudo cd /home/diary');
+        //这一步很关键
+        system('sudo unset GIT_DIR');
+        system('sudo git pull');
+        return 'Hello ly !';
 	}
 
 }
